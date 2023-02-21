c7a6fa4fb90e732480c4ce65cc0c755ca9189f55
letskuku <90572599+letskuku@users.noreply.github.com>
2023-02-22 00:52:57 +0900

    [Silver III] Title: N과 M (1), Time: 20 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+int n, m;
+int arr[10]; //수열 저장
+bool isused[10]; //방문 여부
+
+void func(int k) {
+	//숫자 m개 선택 완료
+	if (k == m) {
+		for (int i = 0; i < m; i++) {
+			cout << arr[i] << ' ';
+		}
+		cout << '\n';
+	
+		return;
+	}
+
+	for (int i = 1; i <= n; i++) {
+		if (!isused[i]) {
+			arr[k] = i;
+			isused[i] = true;
+			func(k + 1);
+			isused[i] = false;
+		}
+	}
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+
+	cin >> n >> m;
+
+	func(0);
+
+	return 0;
+}
