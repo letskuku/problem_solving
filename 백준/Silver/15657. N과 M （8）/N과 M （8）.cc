62b2c6b6be0c2a7aa45ca60b89fad9b9a9ee278f
letskuku <90572599+letskuku@users.noreply.github.com>
2023-02-22 01:34:34 +0900

    [Silver III] Title: N과 M (8), Time: 4 ms, Memory: 2016 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.cc
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int n, m;
+int answer[10];
+int arr[10];
+
+void solve(int level, int num)
+{
+	if (level == m)
+	{
+		for (int i = 0; i < m; i++)
+		{
+			cout << answer[i] << " ";
+		}
+
+		cout << "\n";
+		return;
+	}
+
+	for (int i = num; i < n; i++)
+	{
+		answer[level] = arr[i];
+		solve(level + 1, i);
+	}
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+
+	cin >> n >> m;
+	
+	for (int i = 0; i < n; i++)
+	{
+		cin >> arr[i];
+	}
+
+	sort(arr, arr + n);
+
+	solve(0, 0);
+
+	return 0;
+}
