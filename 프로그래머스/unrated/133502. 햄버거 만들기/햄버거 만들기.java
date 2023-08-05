8de33668c9befc09ae8e54935fa07fd159062eb7
이유정 <90572599+letskuku@users.noreply.github.com>
2023-08-06 03:42:00 +0900

    [unrated] Title: 햄버거 만들기, Time: 0.12 ms, Memory: 81.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] ingredient) {
+        int answer = 0;
+        
+        Stack<Integer> st = new Stack<>();
+        for (int num : ingredient) {
+            st.push(num);
+            
+            if (st.size() >= 4) {
+                if ( (st.get(st.size() - 1) == 1)
+                    && (st.get(st.size() - 2) == 3)
+                    && (st.get(st.size() - 3) == 2)
+                    && (st.get(st.size() - 4) == 1)) {
+                    
+                    answer++;
+                    st.pop();
+                    st.pop();
+                    st.pop();
+                    st.pop();
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
