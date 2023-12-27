69cb31dd17bbe4206be03545d5c86b10fa1e7766
Yujeong Lee <90572599+letskuku@users.noreply.github.com>
2023-12-27 13:57:12 +0900

    [Silver III] Title: 에너지 드링크, Time: 596 ms, Memory: 32896 KB -BaekjoonHub

---
+++ 백준/Silver/20115. 에너지 드링크/에너지 드링크.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+        double ans = 0;
+        List<Long> drinks = new ArrayList<>();
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            drinks.add(Long.parseLong(st.nextToken()));
+        }
+        Collections.sort(drinks);
+
+        ans += drinks.get(n - 1);
+        for (int i = 0; i < n - 1; i++) {
+            ans += ((double) drinks.get(i)) / 2;
+        }
+
+        System.out.println(ans);
+    }
+}
