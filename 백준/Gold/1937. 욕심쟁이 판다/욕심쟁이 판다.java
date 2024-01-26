87de48ba214926d67e36541ead03c4f5796de352
Yujeong Lee <90572599+letskuku@users.noreply.github.com>
2024-01-27 02:00:35 +0900

    [Gold III] Title: 욕심쟁이 판다, Time: 532 ms, Memory: 60084 KB -BaekjoonHub

---
+++ 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.java
@@ -0,0 +1,57 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int n;
+    static int[][] forest, dp;
+    static int[] dx = {0, 0, -1, 1};
+    static int[] dy = {-1, 1, 0, 0};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        n = Integer.parseInt(br.readLine());
+        forest = new int[n][n];
+        dp = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                forest[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                ans = Math.max(ans, dfs(i, j));
+            }
+        }
+
+        System.out.println(ans);
+    }
+
+    static int dfs(int x, int y) {
+        if (dp[x][y] != 0) {
+            return dp[x][y];
+        }
+
+        dp[x][y] = 1;
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if (ny < 0 || ny >= n || nx < 0 || nx >= n) {
+                continue;
+            }
+
+            if (forest[x][y] < forest[nx][ny]) {
+                dp[x][y] = Math.max(dp[x][y], dfs(nx, ny) + 1);
+            }
+        }
+
+        return dp[x][y];
+    }
+}
