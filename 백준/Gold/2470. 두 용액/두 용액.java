8cdee175513edef67f699b4420cc3b5fc8fb2e48
Yujeong Lee <90572599+letskuku@users.noreply.github.com>
2024-07-12 00:36:58 +0900

    [Gold V] Title: 두 용액, Time: 372 ms, Memory: 31412 KB -BaekjoonHub

--- 백준/Gold/2470. 두 용액/두 용액.java
+++ 백준/Gold/2470. 두 용액/두 용액.java
@@ -22,42 +22,34 @@ public class Main {
 
         Collections.sort(list);
 
-        if (list.get(0) > 0) {
-            System.out.println(list.get(0) + " " + list.get(1));
-            return;
-        } else if (list.get(n - 1) < 0) {
-            System.out.println(list.get(n - 2) + " " + list.get(n - 1));
-            return;
-        }
-
         int prev = 2000000001;
+        int start = 0;
         int end = n - 1;
         int[] ans = new int[2];
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = end; j > i; j--) {
-                int tmp = list.get(i) + list.get(j);
+        while (start < end) {
+            int tmp = list.get(start) + list.get(end);
 
-                if (tmp == 0) {
-                    System.out.println(list.get(i) + " " + list.get(j));
-                    return;
-                } else if (tmp > 0) {
-                    if (tmp < prev) {
-                        prev = tmp;
-                        ans[0] = list.get(i);
-                        ans[1] = list.get(j);
-                    }
-                } else {
-                    tmp = Math.abs(tmp);
-
-                    if (tmp < prev) {
-                        prev = tmp;
-                        ans[0] = list.get(i);
-                        ans[1] = list.get(j);
-                    }
-
-                    end = j;
-                    break;
+            if (tmp == 0) {
+                System.out.println(list.get(start) + " " + list.get(end));
+                return;
+            } else if (tmp > 0) {
+                if (tmp < prev) {
+                    prev = tmp;
+                    ans[0] = list.get(start);
+                    ans[1] = list.get(end);
                 }
+                
+                end--;
+            } else {
+                tmp = Math.abs(tmp);
+
+                if (tmp < prev) {
+                    prev = tmp;
+                    ans[0] = list.get(start);
+                    ans[1] = list.get(end);
+                }
+
+                start++;
             }
         }
 
