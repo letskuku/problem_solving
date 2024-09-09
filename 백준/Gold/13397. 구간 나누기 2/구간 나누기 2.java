0c7496c72e46c98bebfcc17e78905ca95d5cd433
Yujeong Lee <90572599+letskuku@users.noreply.github.com>
2024-09-09 20:45:47 +0900

    [Gold IV] Title: 구간 나누기 2, Time: 128 ms, Memory: 14980 KB -BaekjoonHub

--- 백준/Gold/13397. 구간 나누기 2/구간 나누기 2.java
+++ 백준/Gold/13397. 구간 나누기 2/구간 나누기 2.java
@@ -1,50 +1,68 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
 
-	static int n, INF = 987654321;
-	static int[] arr;
-	public static void main(String[] args) throws  IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		n = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(st.nextToken());
-		
-		int left = 0;
-		int right = -1;
-		arr = new int[n];
-		st = new StringTokenizer(br.readLine());
-		for(int i=0; i<n; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-			right = Math.max(right, arr[i]);
-		}
-		
-		while(left < right) {
-			int mid = (left + right)/2;
-			if(solve(mid) <= m) {
-				right = mid;
-			}else {
-				left = mid+1;
-			}
-		}
-		System.out.println(right);
-	}
-	
-	static int solve(int mid) {
-		int cnt = 1;
-		int min = INF;
-		int max = -INF;
-		for(int i=0; i<n; i++) {
-			min = Math.min(min, arr[i]);
-			max = Math.max(max, arr[i]);
-			if(max - min > mid) {
-				cnt++;
-				max = -INF; min = INF;
-				i--;
-			}
-		}
-		return cnt;
-	}
+    static int n;
+    static int[] nums;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        nums = new int[n];
+        int max = Integer.MIN_VALUE;
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+            
+            if (max < nums[i]) {
+                max = nums[i];
+            }
+        }
+
+        int start = 0;
+        int end = max;
+        while (start < end) {
+            int mid = (start + end) / 2;
+
+            if (func(mid) <= m) {
+                end = mid;
+            } else {
+                start = mid + 1;
+            }
+        }
+
+        System.out.println(end);
+    }
+
+    static int func(int mid) {
+        int cnt = 1;
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            if (min > nums[i]) {
+                min = nums[i];
+            }
+            if (max < nums[i]) {
+                max = nums[i];
+            }
+
+            if (max - min > mid) {
+                cnt++;
+                i--;
+
+                min = Integer.MAX_VALUE;
+                max = Integer.MIN_VALUE;
+            }
+        }
+
+        return cnt;
+    }
 }
