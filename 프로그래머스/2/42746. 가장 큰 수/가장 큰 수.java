8ab63f6f50cee1b330f41691b29da3c3b7dad1f6
Yujeong Lee <90572599+letskuku@users.noreply.github.com>
2024-10-12 21:30:25 +0900

    [level 2] Title: 가장 큰 수, Time: 222.31 ms, Memory: 122 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution {
+    public String solution(int[] numbers) {
+        String answer;
+        
+        List<String> list = new ArrayList<>();
+        for (int i = 0; i < numbers.length; i++) {
+            list.add(String.valueOf(numbers[i]));
+        }
+        Collections.sort(list, (s1, s2) -> {
+            int tmp1 = Integer.parseInt(s1 + s2);
+            int tmp2 = Integer.parseInt(s2 + s1);
+            
+            return tmp2 - tmp1;
+        });
+            
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < list.size(); i++) {
+            sb.append(list.get(i));
+        }
+        answer = sb.toString();
+        
+        if (list.get(0).equals("0")) {
+            answer = "0";
+        }
+        
+        return answer;
+    }
+}
